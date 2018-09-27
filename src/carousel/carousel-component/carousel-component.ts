8867efc91cd8a59ca3703fd9b0d13cb927537b5c
Prakash, Ashwani (US - Bengaluru) <ashprakash@deloitte.com>
2018-09-27 15:18:48 +0530

    fix: enabling and disabling of prev/next button

--- src/carousel/carousel-component/carousel-component.ts
+++ src/carousel/carousel-component/carousel-component.ts
@@ -11,23 +11,66 @@ import { animate, AnimationBuilder, AnimationFactory, AnimationPlayer, style } f
     templateUrl: './carousel-component.html',
     styleUrls: ['./carousel-component.scss']
 })
-export class CarouselComponent implements AfterViewInit {
+export class CarouselComponent implements AfterViewInit, OnInit {
     @ContentChildren(CarouselItemDirective) items: QueryList<CarouselItemDirective>;
     @ViewChildren(CarouselItemElementDirective, { read: ElementRef }) private itemsElements: QueryList<ElementRef>;
     @ViewChild('carousel') private carousel: ElementRef;
     @Input() timing = '250ms ease-in';
     @Input() showControls = true;
+
     private player: AnimationPlayer;
     private itemWidth: number;
-    private currentSlide = 0;
-    carouselWrapperStyle = {};
+    private currentSlide = 1;
+    private noOfSlidesInView: number;
+    public disableButton: boolean;
+    public noOfSlides: number;
+    public carouselWrapperStyle = {};
+    public disablePrevButton: boolean;
+
+    constructor(private builder: AnimationBuilder) {
+        this.disableButton = false;
+        this.noOfSlidesInView = 4;
+        this.disablePrevButton = true;
+    }
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        if (this.items.length > 0) {
+            this.noOfSlides = Math.ceil(this.items.length / this.noOfSlidesInView);
+        }
+        this.itemWidth = this.itemsElements.first.nativeElement.getBoundingClientRect().width * this.noOfSlidesInView;
+        this.carouselWrapperStyle = {
+            width: `${this.itemWidth}px`
+        };
+    }
 
     next() {
-        if (this.currentSlide + 1 === this.items.length) {
+        this.currentSlide = this.currentSlide + 1;
+        if (this.currentSlide  === this.noOfSlides) {
+            this.disableButton = true;
+            this.disablePrevButton = false;
             return;
         }
-        this.currentSlide = (this.currentSlide + 1) % this.items.length;
+        this.disableButton = false;
+        this.disablePrevButton = false;
         const offset = this.currentSlide * this.itemWidth;
+        const myAnimation: AnimationFactory = this.buildAnimation(offset);
+        this.player = myAnimation.create(this.carousel.nativeElement);
+        this.player.play();
+    }
+
+    prev() {
+        if (this.currentSlide === 0) {
+            this.disablePrevButton = true;
+            this.disableButton = false;
+            return;
+        }
+        this.disablePrevButton = false;
+        this.disableButton = false;
+        this.currentSlide = this.currentSlide - 1;
+        const offset = this.currentSlide * this.itemWidth;
+
         const myAnimation: AnimationFactory = this.buildAnimation(offset);
         this.player = myAnimation.create(this.carousel.nativeElement);
         this.player.play();
@@ -37,29 +80,6 @@ export class CarouselComponent implements AfterViewInit {
         return this.builder.build([
             animate(this.timing, style({ transform: `translateX(-${offset}px)` }))
         ]);
-    }
-
-    prev() {
-        if (this.currentSlide === 0) {
-            return;
-        }
-
-        this.currentSlide = ((this.currentSlide - 1) + this.items.length) % this.items.length;
-        const offset = this.currentSlide * this.itemWidth;
-
-        const myAnimation: AnimationFactory = this.buildAnimation(offset);
-        this.player = myAnimation.create(this.carousel.nativeElement);
-        this.player.play();
-    }
-
-    constructor(private builder: AnimationBuilder) {
-    }
-
-    ngAfterViewInit() {
-            this.itemWidth = this.itemsElements.first.nativeElement.getBoundingClientRect().width * 3;
-            this.carouselWrapperStyle = {
-                width: `${this.itemWidth}px`
-            };
     }
 
 }
